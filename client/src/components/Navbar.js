cf97a7429b54a4d4d95bc3d3e47330304fccca17
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-06-26 11:59:43 -0500

    changed component of index link to prevent link highlight when not selected

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,34 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
 import { Nav } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
 import "../assets/styles/Navbar.css";
 
-export default class Navbar extends React.Component {
+export default class SideNavbar extends React.Component {
   render() {
     return (
 
-      <Nav className="text-center flex-column justify-content-center" id="sidebar"
-        activeKey="/home"
-        onSelect={(selectedKey) => console.log(`selected ${selectedKey}`)}>
-        <div className="sidebar-sticky"></div>
-        
-        <Image src={require("../assets/images/photo.jpg")} className="d-block mx-auto img-fluid" id="myPic" width={180} roundedCircle />
-        
-        <Nav.Item className="pt-3">
-          <LinkContainer to="/">
-            <Nav.Link>ABOUT</Nav.Link>
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item>
-          <LinkContainer to="/skills">
-            <Nav.Link>SKILLS</Nav.Link>
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item>
-          <LinkContainer to="/projects">
-            <Nav.Link>PROJECTS</Nav.Link>
-          </LinkContainer>
-        </Nav.Item>
-        <Nav.Item>
-          <LinkContainer to="/contact">
-            <Nav.Link>CONTACT</Nav.Link>
-          </LinkContainer>
-        </Nav.Item>
-      </Nav>
+      <Navbar expand="lg" id="sidebar">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="flex-column text-center">
+            <Image src={require("../assets/images/photo.jpg")} className="img-fluid" id="myPic" width={180} roundedCircle />
+            <IndexLinkContainer to="/">
+              <Nav.Link>ABOUT</Nav.Link>
+            </IndexLinkContainer>
+            <LinkContainer to="/skills">
+              <Nav.Link>SKILLS</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/projects">
+              <Nav.Link>PROJECT</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link>CONTACT</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
