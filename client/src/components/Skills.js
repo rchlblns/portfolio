cfaf62858513f2f516504871ff30cb3fad109de8
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-06-29 12:32:53 -0500

    skill icon sizes now responsive

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ListGroup from "react-bootstrap/ListGroup";
-import { DiHtml5, DiReact } from "react-icons/di";
-import { DiCss3 } from "react-icons/di";
-import { DiJavascript1 } from "react-icons/di";
-import { DiJqueryLogo } from "react-icons/di";
-import { DiNodejsSmall } from "react-icons/di";
-import { DiMysql } from "react-icons/di";
-import { DiMaterializecss} from "react-icons/di";
-import { DiBootstrap } from "react-icons/di";
-import { DiPhotoshop } from "react-icons/di";
-import { DiIllustrator } from "react-icons/di";
-import { DiGit } from "react-icons/di";
+import {
+    DiHtml5,
+    DiReact,
+    DiCss3,
+    DiJavascript1,
+    DiJqueryLogo,
+    DiNodejsSmall,
+    DiMysql,
+    DiMaterializecss,
+    DiBootstrap,
+    DiPhotoshop,
+    DiIllustrator,
+    DiGit
+} from "react-icons/di";
 import "../assets/styles/Skills.css";
+import { IconContext } from "react-icons/lib";
 
 
 export default class Skills extends React.Component {
@@ -26,51 +29,69 @@ export default class Skills extends React.Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <ListGroup horizontal>
-                            <ListGroup.Item>
-                                <DiHtml5 size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiCss3 size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiJavascript1 size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiJqueryLogo size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiNodejsSmall size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiMysql size={100}/>
-                            </ListGroup.Item>
-                        </ListGroup>
+                    <Col xs="auto">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiHtml5 size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+
+                            <DiCss3 size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiJavascript1 size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiJqueryLogo size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiNodejsSmall size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiMysql size={"6.0em"} />
+                        </IconContext.Provider>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <ListGroup horizontal>
-                            <ListGroup.Item>
-                                <DiBootstrap size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiMaterializecss size={100} />
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiPhotoshop size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiIllustrator size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiGit size={100}/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <DiReact size={100}/>
-                            </ListGroup.Item>
-                        </ListGroup>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiBootstrap size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiMaterializecss size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiPhotoshop size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiIllustrator size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiGit size={"6.0em"} />
+                        </IconContext.Provider>
+                    </Col>
+                    <Col xs="auto" id="skill-icon">
+                        <IconContext.Provider value={{ className: "react-icons" }}>
+                            <DiReact size={"6.0em"} />
+                        </IconContext.Provider>
                     </Col>
                 </Row>
             </Container>
