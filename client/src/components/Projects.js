55069b2ee5e964f3232e4938edc32c6331a89b76
Short Stack <41342213+rchlblns@users.noreply.github.com>
2020-07-20 13:40:47 -0500

    image sizes compressed to improve load time

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -20,32 +20,32 @@ export default class Projects extends React.Component {
                         <CardColumns >
                             <Card border="light" className="shadow-sm project-card">
                                 <LinkContainer to="/projects/lumikha">
-                                    <Card.Img src={require("../assets/images/lumikha.png")} alt="Lumikha" className="project-pic"/>
+                                    <Card.Img src={require("../assets/images/lumikha.jpg")} alt="Lumikha" className="project-pic"/>
                                 </LinkContainer>
                             </Card>
                             <Card border="light" className="shadow-sm project-card mt-2">
                                 <LinkContainer to="/projects/clicky-game">
-                                    <Card.Img src={require("../assets/images/clicky_game.png")} alt="Clicky Game"className="project-pic"/>
+                                    <Card.Img src={require("../assets/images/clicky_game.jpg")} alt="Clicky Game"className="project-pic"/>
                                 </LinkContainer>
                             </Card>
                             <Card border="light" className="shadow-sm project-card">
                                 <LinkContainer to="/projects/social-animals">
-                                    <Card.Img src={require("../assets/images/social-animals.png")} alt="Social Animals"className="project-pic"/>
+                                    <Card.Img src={require("../assets/images/social-animals.jpg")} alt="Social Animals"className="project-pic"/>
                                 </LinkContainer>
                             </Card>
                             <Card border="light" className="shadow-sm project-card mt-2">
                                 <LinkContainer to="/projects/train-scheduler">
-                                    <Card.Img src={require("../assets/images/trainscheduler.png")} alt="Train Scheduler" className="project-pic" />
+                                    <Card.Img src={require("../assets/images/trainscheduler.jpg")} alt="Train Scheduler" className="project-pic" />
                                 </LinkContainer>
                             </Card>
                             <Card border="light" className="shadow-sm project-card">
                                 <LinkContainer to="/projects/pacer-tms">
-                                    <Card.Img src={require("../assets/images/pacertms.png")} alt="Pacer TMS" className="project-pic" />
+                                    <Card.Img src={require("../assets/images/pacertms.jpg")} alt="Pacer TMS" className="project-pic" />
                                 </LinkContainer>
                             </Card>
                             <Card border="light" className="shadow-sm project-card mt-2">
                                 <LinkContainer to="/projects/trivia-game">
-                                    <Card.Img src={require("../assets/images/hptriviagame.png")} alt="Harry Potter Trivia Game" className="project-pic" />
+                                    <Card.Img src={require("../assets/images/hptriviagame.jpg")} alt="Harry Potter Trivia Game" className="project-pic" />
                                 </LinkContainer>
                             </Card>
                         </CardColumns>
